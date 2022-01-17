6c8d74a972956ce957fd963f62da1ea8bb5ffe0e
David Griffin <david.dtjg2001@gmail.com>
2022-01-17 06:45:28 -0700

    array length dynamically changes based on user inter input and array is random

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,8 @@ var upperCaseLetters = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', '
 var lowerCaseLetters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
 
 // Password criteria
-var passwordPrompt = function () {
+
+function passwordPrompt() {
   var lengthPrompt;
   function pickNumber() {
     var lengthPrompt = parseInt(prompt('Enter an integer for password length with at least 8 characters and no more than 128 characters'));
@@ -19,53 +20,84 @@ var passwordPrompt = function () {
       alert('You need to choose a length of characters from 8 to 128!')
       pickNumber();  // runs the function again from the beginning
     }
+    function chooseCriteria() {
+      // lowercase 
+      var lowercasePrompt = confirm('Do you want lowercase letters in your password?');
+      // uppercase characters
+      var uppercasePrompt = confirm('Do you want uppercase letters in your password?');
+      // numeric characters
+      var numbersPrompt = confirm('Do you want numbers in your password?');
+      // special characters
+      var specialPrompt = confirm('Do you want special characters in your password?');
+      // if the user did not enter enough criteria for their password
+      if (
+        lowercasePrompt === false &&
+        uppercasePrompt === false &&
+        numbersPrompt === false &&
+        specialPrompt === false) {
+        alert('You did not choose enough options, please choose at least one option!')
+        chooseCriteria();
+      };
+  
+      var passwordChoices = {
+        lowercase: lowercasePrompt,
+        uppercase: uppercasePrompt,
+        numbers: numbersPrompt,
+        special: specialPrompt,
+        length: lengthPrompt
+      };
+      console.log(passwordChoices.length)
+      // various password array combinations 
+      var array;
+      var array = []
+      if (lowercasePrompt) {
+        array.push(...(lowerCaseLetters))
+      }
+      if (uppercasePrompt) {
+        array.push(...(upperCaseLetters))
+      }
+      if (specialPrompt) {
+        array.push(...(specialCharacters))
+      }
+      if (numbersPrompt) {
+        array.push(...(numbers))
+      }
+
+
+      function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
+      
+        // While there remain elements to shuffle...
+        while (currentIndex != 0) {
+      
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+      
+        return array;
+      }
+      
+      // Used like so
+      
+      shuffle(array);
+      array.length = lengthPrompt 
+
+      console.log(array);
+
+  
+      
+      
+    };
+    chooseCriteria();
+  
   };
   // console.log(lengthPrompt + 'lowercase: ' + lowercasePrompt + 'uppercase: ' + uppercasePrompt + 'numbers: ' + numbersPrompt + 'special chars: ' + specialPrompt)
   pickNumber();
-
-  function chooseCriteria(lengthPrompt) {
-    // lowercase 
-    var lowercasePrompt = confirm('Do you want lowercase letters in your password?');
-    // uppercase characters
-    var uppercasePrompt = confirm('Do you want uppercase letters in your password?');
-    // numeric characters
-    var numbersPrompt = confirm('Do you want numbers in your password?');
-    // special characters
-    var specialPrompt = confirm('Do you want special characters in your password?');
-    // if the user did not enter enough criteria for their password
-    if (
-      lowercasePrompt === false &&
-      uppercasePrompt === false &&
-      numbersPrompt === false &&
-      specialPrompt === false) {
-      alert('You did not choose enough options, please choose at least one option!')
-      chooseCriteria();
-    };
-
-    var passwordChoices = {
-      lowercase: lowercasePrompt,
-      uppercase: uppercasePrompt,
-      numbers: numbersPrompt,
-      special: specialPrompt,
-      length: lengthPrompt
-    };
-
-    // various password array combinations 
-    const pool = []
-    if (lowercasePrompt) {
-      pool.push(...random(lowerCaseLetters))
-    }
-    if (uppercasePrompt) {
-      pool.push(...random(upperCaseLetters))
-    }
-    if (specialPrompt) {
-      pool.push(...random(specialCharacters))
-    }
-    if (numbersPrompt) {
-      pool.push(...random(numbers))
-    }
-  };
-  chooseCriteria(lengthPrompt);
 }
 
 
@@ -73,25 +105,26 @@ var passwordPrompt = function () {
 
 
 
-function generatePassword() { }
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate')
 generateBtn.addEventListener('click', passwordPrompt)
 
-function random(array) {
-  return array[Math.floor(Math.random() * array.length)]
-}
 
 
-
+function generatePassword() {}
 // Write password to the #password input
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
+  passwordText.value = 'hello';}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
