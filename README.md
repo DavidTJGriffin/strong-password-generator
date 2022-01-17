6690c54acb7b25bb328bc13d7b35da3f4365bfaf
David Griffin <david.dtjg2001@gmail.com>
2022-01-16 20:34:28 -0700

    changed file structure and updated prompts to improve user experience

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# Password Generator Starter Code
+# <Your-Project-Title>
+## Description
+- I created this project so that people had access to a password generator they use to help improve their security. 
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+## Installation
+Open the link: https://davidtjgriffin.github.io/strong-password-generator/
+## Usage
+Click "Generate Password" button to open prompts that let you choose the specific criteria you want your generated password to have. Once you have answered all of the prompts, you will get a password generated in the box!
+
+
+
+
+![Website Screenshot](assets/images/website-screenshot.jpg)
+## Credits
+[David Griffin](https://github.com/Ukarimu1)
+## License
+MIT License
+
+Copyright © 2021 David Griffin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+![MIT License](https://img.shields.io/apm/l/vim-mode)
+
+## Features
+- Special characters
+- Numbers
+- Uppercase letters
+- Lowercase letters
+## How to Contribute
+1. Fork the project to your own machine
+2. Refactor code to be further optimized 
+3. Commit changes to your own branch
+4. Push your work back up to your branch
+5. Submit a Pull request so that I can review your changes
+## Tests
+- Choose different criteria and test to see what types of characters show up in your generated password
---
+++ assets/js/script.js
@@ -0,0 +1,68 @@
+// Assignment code here
+var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+var capitalLetters = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'X', 'C', 'V', 'B', 'N', 'M']
+
+var lowerCaseLetters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
+
+var allCharacters = specialCharacters.concat(numbers, capitalLetters, lowerCaseLetters)
+
+// prompts for various password criteria
+var passwordPrompt = function () {
+
+  function pickNumber() {
+    var lengthPrompt = parseInt(prompt('Enter an integer for password length with at least 8 characters and no more than 128 characters'));
+
+    if (lengthPrompt >= 8 && lengthPrompt <= 128) {
+      alert('You choose a length of ' + lengthPrompt + ' characters.');
+    } else {
+      alert('You need to choose a length of characters from 8 to 128!')
+      pickNumber();  // runs the function again from the beginning
+    }
+  };
+  // console.log(lengthPrompt + 'lowercase: ' + lowercasePrompt + 'uppercase: ' + uppercasePrompt + 'numbers: ' + numericPrompt + 'special chars: ' + specialPrompt)
+  pickNumber();
+
+  function chooseCriteria () {
+    // lowercase 
+    var lowercasePrompt = confirm('Do you want lowercase letters in your password?');
+    // uppercase characters
+    var uppercasePrompt = confirm('Do you want uppercase letters in your password?');
+    // numeric characters
+    var numericPrompt = confirm('Do you want numbers in your password?');
+    // special characters
+    var specialPrompt = confirm('Do you want special characters in your password?');
+
+    if (
+      lowercasePrompt === false &&
+      uppercasePrompt === false &&
+      numericPrompt === false &&
+      specialPrompt === false) {
+      alert('You did not choose enough options, please choose at least one option!')
+      chooseCriteria();
+    };
+  };
+
+
+  // if the user did not enter enough criteria for their password
+
+}
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate')
+generateBtn.addEventListener('click', passwordPrompt)
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
