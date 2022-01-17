77e7c3f9f0b353704b6917a34dc8ffe987eefea1
David Griffin <david.dtjg2001@gmail.com>
2022-01-17 00:01:07 -0700

    added initial array combinations for password

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,13 @@ var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
 
 var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-var capitalLetters = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'X', 'C', 'V', 'B', 'N', 'M']
+var upperCaseLetters = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'X', 'C', 'V', 'B', 'N', 'M']
 
 var lowerCaseLetters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
 
-var allCharacters = specialCharacters.concat(numbers, capitalLetters, lowerCaseLetters)
-
 // Password criteria
 var passwordPrompt = function () {
-
+  var lengthPrompt;
   function pickNumber() {
     var lengthPrompt = parseInt(prompt('Enter an integer for password length with at least 8 characters and no more than 128 characters'));
 
@@ -22,24 +20,23 @@ var passwordPrompt = function () {
       pickNumber();  // runs the function again from the beginning
     }
   };
-  // console.log(lengthPrompt + 'lowercase: ' + lowercasePrompt + 'uppercase: ' + uppercasePrompt + 'numbers: ' + numericPrompt + 'special chars: ' + specialPrompt)
+  // console.log(lengthPrompt + 'lowercase: ' + lowercasePrompt + 'uppercase: ' + uppercasePrompt + 'numbers: ' + numbersPrompt + 'special chars: ' + specialPrompt)
   pickNumber();
 
-  function chooseCriteria () {
+  function chooseCriteria(lengthPrompt) {
     // lowercase 
     var lowercasePrompt = confirm('Do you want lowercase letters in your password?');
     // uppercase characters
     var uppercasePrompt = confirm('Do you want uppercase letters in your password?');
     // numeric characters
-    var numericPrompt = confirm('Do you want numbers in your password?');
+    var numbersPrompt = confirm('Do you want numbers in your password?');
     // special characters
     var specialPrompt = confirm('Do you want special characters in your password?');
-
-// if the user did not enter enough criteria for their password
+    // if the user did not enter enough criteria for their password
     if (
       lowercasePrompt === false &&
       uppercasePrompt === false &&
-      numericPrompt === false &&
+      numbersPrompt === false &&
       specialPrompt === false) {
       alert('You did not choose enough options, please choose at least one option!')
       chooseCriteria();
@@ -48,25 +45,42 @@ var passwordPrompt = function () {
     var passwordChoices = {
       lowercase: lowercasePrompt,
       uppercase: uppercasePrompt,
-      numbers: numericPrompt,
+      numbers: numbersPrompt,
       special: specialPrompt,
       length: lengthPrompt
     };
+
+    // various password array combinations 
+    const pool = []
+    if (lowercasePrompt) {
+      pool.push(...random(lowerCaseLetters))
+    }
+    if (uppercasePrompt) {
+      pool.push(...random(upperCaseLetters))
+    }
+    if (specialPrompt) {
+      pool.push(...random(specialCharacters))
+    }
+    if (numbersPrompt) {
+      pool.push(...random(numbers))
+    }
   };
-  chooseCriteria();
-
+  chooseCriteria(lengthPrompt);
 }
-function generatePassword() {
-  var passwordChoices = passwordPrompt();
 
 
 
 
-  
-}
+
+
+function generatePassword() { }
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate')
 generateBtn.addEventListener('click', passwordPrompt)
+
+function random(array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
 
 
 
